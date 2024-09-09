3aee536041dd939ff9ea6fb10644f232d22810c4
Willian <williank97@hotmail.com>
2024-09-09 15:54:35 -0300

    init

---
+++ scripts/lang.js
@@ -0,0 +1,48 @@
+// scripts/lang.js
+
+function loadTranslations(lang) {
+    fetch('scripts/translations.json')
+        .then(response => response.json())
+        .then(translations => {
+            applyTranslations(translations[lang]);
+        })
+        .catch(error => console.error('Erro ao carregar traduções:', error));
+}
+
+function applyTranslations(translations) {
+    document.getElementById('welcomeMessage').innerHTML = translations.welcome;
+    document.getElementById('inputIdade').placeholder = translations.age;
+    document.getElementById('inputAltura').placeholder = translations.height;
+    document.getElementById('inputPeso').placeholder = translations.weight;
+    document.getElementById('inputPesoDesejado').placeholder = translations.desiredWeight;
+    document.getElementById('activityQuestion').textContent = translations.activityQuestion;
+    document.getElementById('genderQuestion').textContent = translations.genderQuestion;
+    document.querySelector('label[for="Masculino"]').textContent = translations.male;
+    document.querySelector('label[for="Femenino"]').textContent = translations.female;
+
+    const select = document.getElementById('Actividad');
+    while (select.firstChild) {
+        select.removeChild(select.firstChild);
+    }
+    const activities = translations.activities
+    activities.forEach((item, index) => {
+        const option = document.createElement('option');
+        option.value = "valor" + (index + 1);
+        option.textContent = item;
+        select.appendChild(option);
+    });
+
+    document.querySelector('button[type="submit"]').textContent = translations.submit;
+}
+
+// Detecta o idioma do navegador ou usa 'es' (Espanhol) como padrão
+const userLang = navigator.language || navigator.userLanguage || 'es';
+if (userLang.startsWith('fr')) {
+    loadTranslations('fr');
+} else if (userLang.startsWith('pt')) {
+    loadTranslations('pt');
+} else if (userLang.startsWith('en')) {
+    loadTranslations('en');
+} else {
+    loadTranslations('es'); // Default para Espanhol
+}
