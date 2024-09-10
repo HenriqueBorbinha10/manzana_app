e5cf5a9f3f4e606622f0ba96040b63cf24f3d69c
Willian <williank97@hotmail.com>
2024-09-10 13:48:46 -0300

    ajustes ebook

---
+++ scripts/lang_index_ebook.js
@@ -0,0 +1,38 @@
+// scripts/lang_index.js
+
+function loadTranslations(lang) {
+    fetch('scripts/translations_ebook.json')
+        .then(response => response.json())
+        .then(translations => {
+            applyTranslations(translations[lang]);
+        })
+        .catch(error => console.error('Erro ao carregar traduções:', error));
+}
+
+function applyTranslations(translations) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const listLangsSuport = ["es", "fr"];
+    const ebook = urlParams.get('ebook');
+    const navigatorLang = (navigator.language || navigator.userLanguage || 'es').split('-')[0];
+    const lang = listLangsSuport.includes(navigatorLang) ? navigatorLang : 'es';
+    
+    document.getElementById('pageTitle').textContent = translations.pageTitle;
+    document.getElementById('backButton').textContent = translations.backButton;
+    document.getElementById('mainHeading').innerHTML = translations.mainHeading;
+    document.getElementById('ebookText').textContent = translations.ebookText;
+
+    if (ebook) {
+      document.getElementById('ebook_pdf').href = `ebooks/${lang}/${ebook}.pdf`;
+      document.getElementById('ebook_html').src = `ebooks/${lang}/${ebook}/${ebook}.html`;
+    }
+}
+
+// Detecta o idioma do navegador ou usa 'es' (Espanhol) como padrão
+const userLang = navigator.language || navigator.userLanguage || 'es';
+if (userLang.startsWith('fr')) {
+    loadTranslations('fr');
+} else if (userLang.startsWith('pt')) {
+    loadTranslations('pt');
+} else {
+    loadTranslations('es'); // Default para Espanhol
+}
