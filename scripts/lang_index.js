7b22cfcc0818b1d401acc25d432ca40b0d45e70f
Willian <williank97@hotmail.com>
2024-10-04 15:27:55 -0300

    ajustes novas imagens

--- scripts/lang_index.js
+++ scripts/lang_index.js
@@ -9,6 +9,17 @@ function loadTranslations(lang) {
         .catch(error => console.error('Erro ao carregar traduções:', error));
 }
 
+function loadImages(lang) {
+    const pathImages = {
+        "pt": "assets/images_pt/",
+        "es": "assets/images/",
+        "fr": "assets/images/",
+    }
+
+    document.getElementById('logo').href = pathImages[lang] + "logo.png";
+    document.getElementById('logo1').src = pathImages[lang] + "logo.png";
+}
+
 function applyTranslations(translations) {
     document.getElementById('welcomeMessage').innerHTML = translations.welcome;
     document.getElementById('inputIdade').placeholder = translations.age;
@@ -39,8 +50,11 @@ function applyTranslations(translations) {
 const userLang = navigator.language || navigator.userLanguage || 'es';
 if (userLang.startsWith('fr')) {
     loadTranslations('fr');
+    loadImages('fr');
 } else if (userLang.startsWith('pt')) {
     loadTranslations('pt');
+    loadImages('pt');
 } else {
     loadTranslations('es'); // Default para Espanhol
+    loadImages('es');
 }
--- scripts/lang_index_ebook.js
+++ scripts/lang_index_ebook.js
@@ -9,9 +9,20 @@ function loadTranslations(lang) {
         .catch(error => console.error('Erro ao carregar traduções:', error));
 }
 
+function loadImages(lang) {
+    const pathImages = {
+        "pt": "assets/images_pt/",
+        "es": "assets/images/",
+        "fr": "assets/images/",
+    }
+    
+    document.getElementById('logoSemFundo').src = pathImages[lang] + "logoSemFundo.png";
+    document.getElementById('logo').href = pathImages[lang] + "logo.png";
+}
+
 function applyTranslations(translations) {
     const urlParams = new URLSearchParams(window.location.search);
-    const listLangsSuport = ["es", "fr"];
+    const listLangsSuport = ["es", "fr", "pt"];
     const ebook = urlParams.get('ebook');
     const navigatorLang = (navigator.language || navigator.userLanguage || 'es').split('-')[0];
     const lang = listLangsSuport.includes(navigatorLang) ? navigatorLang : 'es';
@@ -29,12 +40,14 @@ function applyTranslations(translations) {
     }
 }
 
-// Detecta o idioma do navegador ou usa 'es' (Espanhol) como padrão
 const userLang = navigator.language || navigator.userLanguage || 'es';
 if (userLang.startsWith('fr')) {
     loadTranslations('fr');
+    loadImages('fr');
 } else if (userLang.startsWith('pt')) {
     loadTranslations('pt');
+    loadImages('pt');
 } else {
     loadTranslations('es'); // Default para Espanhol
+    loadImages('es');
 }
